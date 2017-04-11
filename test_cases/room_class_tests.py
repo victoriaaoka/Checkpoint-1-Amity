d02ba3ee6bd9ba97c3ea892f84d36940bbf984a8
victoria <victoriaaoka@gmail.com>
2017-04-11 04:02:59 +0300

    Add Room class tests

---
+++ test_cases/room_class_tests.py
@@ -0,0 +1,33 @@
+import unittest
+from ..my_app.room_class import *
+
+
+class TestRoom(unittest.TestCase):
+    def setUp(self):
+        self.office_space = OfficeSpace("Blue")
+        self.living_space = LivingSpace("Emerald")
+        
+    def test_officespace_is_a_room_instance(self):
+        self.assertIsInstance(self.office_space, Room, msg = "The object should be an instance of the Room class")
+
+    def test_livingspace_is_a_room_instance(self):
+        self.assertIsInstance(self.living_space, Room, msg = "The object should be an instance of the Room class")
+
+    def test_livingspace_type(self):
+        self.assertEqual(self.living_space.room_type, "Living Space", msg = "Room Created should be of type 'Living Space'")
+
+    def test_max_size_of_living_space(self):
+        self.assertEqual(self.living_space.capacity, 4, msg = 'The Maximum capacity is 4 occupants')
+
+    def test_officespace_room_type(self):
+        self.assertEqual(self.office_space.room_type, "Office", msg = "Room created should be of type Office")
+
+    def test_max_size_of_office_space(self):
+        self.assertEqual(self.office_space.capacity, 6, msg ='The Maximum capacity is 6 occupants')
+
+    def test_raise_error_if_input_is_not_string(self):
+    	with self.assertRaises(TypeError, msg = 'Use string input only'):
+    		Blue = Room(100)
+    
+if __name__ == '__main__':
+    unittest.main()
