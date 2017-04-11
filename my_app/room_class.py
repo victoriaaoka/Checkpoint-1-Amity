4532716b0b7d4c54adeadc2a107138f1bc0416f6
victoria <victoriaaoka@gmail.com>
2017-04-11 20:10:02 +0300

    add create_room method

--- my_app/room_class.py
+++ my_app/room_class.py
@@ -1,27 +1,49 @@
 class Room(object):
-	"""Room class describes the characteristics of each 
-	instance of a room created
-	"""
-	def __init__(self, name):
-		self.name = name
-		self.occupants = []
+        """Room class describes the characteristics of each
+        instance of a room created
+        """
+        def __init__(self, room_type = "", room_name = ""):
+                self.room_name = room_name
+                self.room_type = room_type
+                self.all_rooms = []
+
+        def create_room(self, room_type, room_name):
+                self.room_type = room_type
+                self.room_name = room_name
+                self.all_rooms.append(self)
+                return self
 
 
 class OfficeSpace(Room):
-	"""This class defines an instance of each Office
-	and inherits from Room class
-	"""
-	def __init__(self, name):
-		super(OfficeSpace, self).__init__(name)
-		self.room_type = "OFFICE"
-		self.capacity = 6	
-		
-
+        """This class defines an instance of each Office
+        and inherits from Room class
+        """
+        def __init__(self, room_type = "", room_name = "" ):
+                super(OfficeSpace, self).__init__(room_type, room_name)
+                self.room_type = "OFFICE"
+                self.room_name = room_name
+                
+                
+        def create_room(self, room_type, room_name):
+                self.room_type = room_type
+                self.room_name = room_name
+                self.all_rooms.append(self)
+                self.capacity = 6
+                return self
 class LivingSpace(Room):
-	"""This class defines an instance of each Livingspace
-	and inherits from Room class
-	"""
-	def __init__(self, name):
-		super(LivingSpace, self).__init__(name)
-		self.room_type = "LIVING SPACE"
-		self.capacity = 4
+        """This class defines an instance of each Livingspace
+        and inherits from Room class
+        """
+        def __init__(self,room_type = "", room_name = ""):
+                super(LivingSpace, self).__init__(room_name)
+                self.room_type = "LIVING SPACE"
+                self.room_name = room_name
+
+        def create_room(self, room_type, room_name):
+                self.room_type = room_type
+                self.room_name = room_name
+                self.capacity = 4
+                self.all_rooms.append(self)
+                return self
+
+
