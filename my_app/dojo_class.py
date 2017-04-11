e0b6f52bbbb144dbc071dabd83db1f0df89a6985
victoria <victoriaaoka@gmail.com>
2017-04-11 23:30:03 +0300

    Implement inheritance for add_person method in Dojo class

--- my_app/dojo_class.py
+++ my_app/dojo_class.py
@@ -1,9 +1,10 @@
 from room_class import Room, OfficeSpace, LivingSpace
+from person_class import Person, Fellow, Staff
 class Dojo():
     
     def __init__(self):
-        self.fellows = []
-        self.staff = []
+        self.all_fellows = []
+        self.all_staff = []
         self.offices = []
         self.livingspaces = []
 
@@ -31,9 +32,36 @@ class Dojo():
             elif room_type.lower() != "office" or room_type.lower() != "living_space":
                 return "Wrong room_type! rooms can only be offices or living spaces"
         else:
-            return "Error"           
+            return "Error"
 
-my_room = Dojo()
-my_room.create_room("office", "Acacia")
-print (my_room.create_room("Living Space", "Tsavo"))
+    def add_person(self, person_id, name, person_type, wants_accom = "N"):
+            """This function adds a new fellow or staff into the system"""            
+            if not isinstance(person_id, str) or not isinstance(name,str) or not isinstance(person_type,str):
+                raise TypeError("Use input type of string only")
+            
+            elif isinstance(person_id, str) and isinstance(name, str) and isinstance(person_type, str):
+                if person_type.lower() == "fellow":
+                    new_fellow = Fellow()
+                    added_fellow = new_fellow.add_person(person_id, name, person_type, wants_accom)
+                    self.all_fellows.append(added_fellow)
+                    print ("Fellow " + name +" has been successfully added.")
+
+                elif person_type.lower() == "staff":
+                    new_staff = Staff()
+                    added_staff = new_staff.add_person(person_id, name, person_type)
+                    self.all_staff.append(added_staff)
+                    print ("Staff " + name + "  has been successfully added.")                    
+                    
+                    
+                    
+                    
+
+dojo = Dojo()
+dojo.create_room("office", "Acacia")
+dojo.create_room("Living Space", "Tsavo")
+dojo.add_person("AND100", "Aoka", "Fellow", "Y")
+dojo.add_person("AND105", "Victoria", "Staff")
+
+
+
 
