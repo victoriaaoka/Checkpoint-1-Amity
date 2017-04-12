4510f07cd646dc6697555d9222032c2a24598409
victoria <victoriaaoka@gmail.com>
2017-04-12 18:41:14 +0300

    Remove repetition

--- my_app/dojo_class.py
+++ my_app/dojo_class.py
@@ -15,26 +15,23 @@ class Dojo():
         Rooms can either be an office or a living space.
         """
         if not isinstance(room_type, str) or not isinstance(room_name,str):
-            raise TypeError("Use input of type string only")
+            raise TypeError("\n\nUse input of type string only\n\n")
         
         elif isinstance(room_type,str) and isinstance(room_name,str):
             if room_type.lower() == "office":
-                new_office = OfficeSpace()
-                created_office = new_office.create_room(room_type, room_name)
+                new_office = OfficeSpace(room_type, room_name)
                 self.offices.append(new_office)
-                print ("An office called " + room_name + " has been successfully created!")
-                #print (created_office.room_name)
-                #print (created_office.capacity)
+                print ("\n\nAn office called " + room_name + " has been successfully created!\n\n")
                 return self
 
-            elif room_type.lower() == "living space":
-                new_livingspace = LivingSpace()
-                created_livingspace = new_livingspace.create_room(room_type, room_name)
-                self.livingspaces.append(created_livingspace)
-                print ("A living space called " +room_name+" has been successfully created")
+            elif room_type.lower() == "livingspace":
+                new_livingspace = LivingSpace(room_type, room_name)
+                self.livingspaces.append(new_livingspace)
+                print ("\n\nA living space called " +room_name+" has been successfully created\n\n")
+                return self
                 
             elif room_type.lower() != "office" or room_type.lower() != "living_space":
-                return "Wrong room_type! rooms can only be offices or living spaces"
+                return "\n\nWrong room_type! rooms can only be offices or living spaces\n\n"
         else:
             return "Error"
 
@@ -43,51 +40,44 @@ class Dojo():
         It also calls the allocate_livingspace and allocate_office method to assign the new staff and fellow rooms.
         """            
         if not isinstance(person_id, str) or not isinstance(name,str) or not isinstance(person_type,str):
-            raise TypeError("Use input type of string only")
+            raise TypeError("\n\nUse input type of string only\n\n")
             
         elif isinstance(person_id, str) and isinstance(name, str) and isinstance(person_type, str):
             if person_type.lower() == "fellow":
-                new_fellow = Fellow()
-                added_fellow = new_fellow.add_person(person_id, name, person_type, wants_accom)
-                self.all_fellows.append(added_fellow)
-                print ("Fellow " + name +" has been successfully added.")
+                new_fellow = Fellow(person_id, name, person_type, wants_accom)
+                self.all_fellows.append(new_fellow)
+                print ("\n\nFellow " + name +" has been successfully added.\n\n")
                 self.allocate_office(name)
                 if wants_accom == "Y":
                     self.allocate_livingspace(name)
 
             elif person_type.lower() == "staff":
-                new_staff = Staff()
-                added_staff = new_staff.add_person(person_id, name, person_type)
-                self.all_staff.append(added_staff)
-                print ("Staff " + name + "  has been successfully added.")
+                new_staff = Staff(person_id, name, person_type)
+                self.all_staff.append(new_staff)
+                print ("\n\nStaff " + name + "  has been successfully added.\n\n")
                 self.allocate_office(name)
                     
     def allocate_office(self, name):
         """This function allocates a random office to a fellow and staff"""
-        for office in self.offices:
+        if len(self.offices) == 0:
+            print ("\n\nThere are no rooms created yet\n\n")
             
-            if len(self.offices) == 0:
-                print ("There are no rooms created yet")
-
-            elif len(self.offices) > 0:
+        for office in self.offices:
+            if len(self.offices) > 0:
                 available_offices = []
                 if len(office.occupants) < office.capacity:
                     available_offices.append(office)
                     selected_office = random.choice(available_offices)
                     selected_office.occupants.append(name)
-                    print (name + " has been allocated the office " + office.room_name)
-                    
-                elif len(office.occupants) == office.capacity:
-                    full_offices = []
-                    full_offices.append(office)
-                    self.offices.remove(office)
+                    print ("\n\n" +name + " has been allocated the office " + office.room_name +"\n\n")
+
 
     def allocate_livingspace(self, name):
         """" This method allocates a living space to fellows who want accommodation"""
         for livingspace in self.livingspaces:
             
             if len(self.livingspaces) == 0:
-                print ("There are no livingspaces created yet")
+                print ("\n\nThere are no livingspaces created yet\n\n")
 
             elif len(self.livingspaces) > 0:
                 available_livingspace = []
@@ -95,18 +85,12 @@ class Dojo():
                     available_livingspace.append(livingspace)
                     selected_livingspace = random.choice(available_livingspace)
                     selected_livingspace.occupants.append(name)
-                    print (name + " has been allocated the livingspace " + livingspace.room_name)
-                    
-                elif len(livingspace.occupants) == livingspace.capacity:
-                    full_livingspaces = []
-                    full_livingspaces.append(livingspace)
-                    self.livingspaces.remove(livingspace)    
+                    print ("\n\n"+ name + " has been allocated the livingspace " + livingspace.room_name+"\n\n")  
                     
                     
-
 dojo = Dojo()
-dojo.create_room("office", "Acacia")
-dojo.create_room("Living Space", "Tsavo")
+#dojo.create_room("office", "Acacia")
+#dojo.create_room("LivingSpace", "Tsavo")
 dojo.add_person("AND100", "Aoka", "Fellow", "Y")
 dojo.add_person("AND105", "Victoria", "Staff")
 
