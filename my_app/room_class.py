e8f2b2b9df31960691a1a9b8c216fb9fe08f860a
victoria <victoriaaoka@gmail.com>
2017-04-12 18:43:06 +0300

    Remove repetition by removing the create_room method

--- my_app/room_class.py
+++ my_app/room_class.py
@@ -7,33 +7,20 @@ class Room(object):
                 self.room_type = room_type
                 self.occupants = []
                 self.all_rooms = []
-
-        def create_room(self, room_type, room_name):
-                self.room_type = room_type
-                self.room_name = room_name
-                self.occupants = []
                 self.all_rooms.append(self)
-                return self
-
-
+                #return self
+        
 class OfficeSpace(Room):
         """This class defines an instance of each Office
         and inherits from Room class
         """
         def __init__(self, room_type = "", room_name = "" ):
                 super(OfficeSpace, self).__init__(room_type, room_name)
-                self.room_type = "OFFICE"
-                self.room_name = room_name
-                self.occupants = []
-                                
-                
-        def create_room(self, room_type, room_name):
                 self.room_type = room_type
                 self.room_name = room_name
-                self.all_rooms.append(self)
-                self.capacity = 6
                 self.occupants = []
-                return self
+                self.capacity = 6
+                #return self                
         
 class LivingSpace(Room):
         """This class defines an instance of each Livingspace
@@ -41,16 +28,12 @@ class LivingSpace(Room):
         """
         def __init__(self,room_type = "", room_name = ""):
                 super(LivingSpace, self).__init__(room_name)
-                self.room_type = "LIVING SPACE"
-                self.room_name = room_name
-                self.occupants = []
-
-        def create_room(self, room_type, room_name):
                 self.room_type = room_type
                 self.room_name = room_name
-                self.capacity = 4
                 self.occupants = []
+                self.capacity = 4
                 self.all_rooms.append(self)
-                return self
+                #return self
+                
 
 
