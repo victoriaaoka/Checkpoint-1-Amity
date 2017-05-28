1c3b5e167fc923f52a14bf866f8a4816b7eba7c4
victoriaaoka <victoria.aoka@andela.com>
2017-05-28 21:19:58 +0300

    Add database directory.

---
+++ db/amity_database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class Person(Base):
+    __tablename__ = "People"
+    id = Column(String, primary_key= True)
+    names = Column(String)
+    person_type = Column(String)
+    wants_accommodation = Column(String)
+    room_allocated = Column(String)
+
+
+class Room(Base):
+    __tablename__ = "Rooms"
+    id = Column(Integer, primary_key= True, autoincrement=True)
+    room_name = Column(String)
+    room_type = Column(String)
+    occupants = Column(String)
+
+
+engine = create_engine("sqlite:///database.db")
+Base.metadata.create_all(engine)
