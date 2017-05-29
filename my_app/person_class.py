653a4fe421f5c2bd525cdb52429fb5996217a6b3
victoriaaoka <victoria.aoka@andela.com>
2017-05-29 11:48:46 +0300

    Rename 'name' to 'person_name'.

--- my_app/person_class.py
+++ my_app/person_class.py
@@ -2,9 +2,9 @@
 class Person(object):
 	"""This class describes the instance of person
 	"""
-	def __init__(self,person_id = "", name = "", person_type = ""):
+	def __init__(self,person_id = "", person_name = "", person_type = ""):
 		self.person_id = person_id
-		self.name = name
+		self.person_name = person_name
 		self.person_type = person_type
 		self.people = []
 
@@ -14,10 +14,10 @@ class Fellow(Person):
 	"""This class inherits from the Person class
         and defines an instance of fellow
 	"""
-	def __init__(self, person_id = "", name = "", person_type = "", wants_accom = ""):
-		super(Fellow, self).__init__(person_id, name, person_type)
+	def __init__(self, person_id = "", person_name = "", person_type = "", wants_accom = ""):
+		super(Fellow, self).__init__(person_id, person_name, person_type)
 		self.person_id = person_id
-		self.name = name
+		self.person_name = person_name
 		self.person_type = "Fellow"
 		self.wants_accom = wants_accom
 
@@ -26,9 +26,10 @@ class Staff(Person):
 	"""This class inherits from the Person class
         and defines an instance of Person
 	"""
-	def __init__(self, person_id = "", name = "", person_type = ""):
-		super(Staff, self).__init__(person_id, name, person_type)
+	def __init__(self, person_id = "", person_name = "", person_type = "", wants_accom = "N"):
+		super(Staff, self).__init__(person_id, person_name, person_type)
 		self.person_id = person_id
-		self.person_type = person_type
+		self.person_name = person_name
 		self.person_type = "Staff"
+		self.wants_accom = "N"
 
