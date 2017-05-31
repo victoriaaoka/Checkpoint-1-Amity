62f2ec0183c2bb968f75c61edac5d040a2e036cd
victoriaaoka <victoria.aoka@andela.com>
2017-05-31 10:24:22 +0300

    Remove unnecessary code

--- my_app/room_class.py
+++ my_app/room_class.py
@@ -7,15 +7,13 @@ class Room(object):
                 self.room_name = room_name
                 self.room_type = room_type
                 self.occupants = []
-                self.all_rooms = []
-                self.all_rooms.append(self)
                 #return self
 
 class OfficeSpace(Room):
         """This class defines an instance of each Office
         and inherits from Room class
         """
-        def __init__(self, room_type = "", room_name = "" ):
+        def __init__(self, room_type = "", room_name = "", occupants=[]):
                 super(OfficeSpace, self).__init__(room_type, room_name)
                 self.room_type = room_type
                 self.room_name = room_name
@@ -27,13 +25,12 @@ class LivingSpace(Room):
         """This class defines an instance of each Livingspace
         and inherits from Room class
         """
-        def __init__(self,room_type = "", room_name = ""):
+        def __init__(self,room_type = "", room_name = "", occupants=[]):
                 super(LivingSpace, self).__init__(room_name)
                 self.room_type = room_type
                 self.room_name = room_name
                 self.occupants = []
                 self.capacity = 4
-                self.all_rooms.append(self)
                 #return self
 
 
