a2efe77d9afaf65b3ad38339cd924a9d67986c88
victoriaaoka <victoria.aoka@andela.com>
2017-06-06 22:47:40 +0300

    Edit to pep8 styling.

--- db/amity_database.py
+++ db/amity_database.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 
 class PersonDb(Base):
     __tablename__ = "People"
-    person_id = Column(String, primary_key= True)
+    person_id = Column(String, primary_key=True)
     person_name = Column(String)
     person_type = Column(String)
     wants_accommodation = Column(String)
@@ -17,7 +17,7 @@ class PersonDb(Base):
 
 class RoomDb(Base):
     __tablename__ = "Rooms"
-    id = Column(Integer, primary_key= True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     room_type = Column(String)
     room_name = Column(String)
     occupants = Column(String)
