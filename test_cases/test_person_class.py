214317f62f2f3d842b3115639f6c5cd6881ebdc1
victoriaaoka <victoria.aoka@andela.com>
2017-06-06 23:12:14 +0300

    Edit to pep8 styling.

--- test_cases/test_person_class.py
+++ test_cases/test_person_class.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
-sys.path.append('../')
 from my_app.person_class import Person, Staff, Fellow
+
 
 class TestPerson(unittest.TestCase):
     def setUp(self):
@@ -9,13 +9,12 @@ class TestPerson(unittest.TestCase):
         self.staff = Staff("Isaiah", "fellowstaff")
 
     def test_fellow_person_type(self):
-        self.assertEqual( self.fellow.person_type, "Fellow" ,
-                          msg = "Invalid person_type specified")
+        self.assertEqual(self.fellow.person_type, "Fellow",
+            msg="Invalid person_type specified")
 
     def test_staff_person_type(self):
-        self.assertEqual( self.staff.person_type, "Staff" ,
-                          msg = "Invalid person_type specified")
-
+        self.assertEqual(self.staff.person_type, "Staff",
+            msg="Invalid person_type specified")
 
 if __name__ == '__main__':
     unittest.main()
